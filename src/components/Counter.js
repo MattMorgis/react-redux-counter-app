8ac93ddd70872bfd819201d15a28107cddd07f52
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-05-09 09:43:29 -0400

    Dispatching counter actions and passing them to the component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,17 +4,19 @@ import PropTypes from 'prop-types';
 class Counter extends React.Component {
   static propTypes = {
     value: PropTypes.number.isRequired,
+    increment: PropTypes.func.isRequired,
+    decrement: PropTypes.func.isRequired
   }
 
   render() {
-    const { value } = this.props
+    const { value, increment, decrement } = this.props
     return (
       <div>
         <p>
           Clicked: {value} times
         </p>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './components/Counter';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Counter from './components/Counter'
+import { createStore } from 'redux'
+import counter from './reducers'
 
-ReactDOM.render(
+const store = createStore(counter)
+
+const render = () => ReactDOM.render(
   <Counter
-    value={0}
+    value={store.getState()}
+    increment={() => store.dispatch({ type: 'INCREMENT' })}
+    decrement={() => store.dispatch({ type: 'DECREMENT' })}
   />,
   document.getElementById('root')
 );
+
+render()
+store.subscribe(render)
